2bc39e2efd3c24231ce0c96bb89d248de4a1428a
C0A24267 <c0a24267dd@edu.teu.ac.jp>
2025-09-30 14:49:52 +0900

    練習4:こうかとんと爆弾の衝突判定

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -15,10 +15,15 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
 def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    引数：こうかとんRect or ばくだんRect
+    戻り値：判定結果タプル（横方向，縦方向）
+    画面内ならTrue/画面外ならFalse
+    """
     yoko, tate = True, True
-    if rct.left < 0 or WIDTH < rct.right:  
+    if rct.left < 0 or WIDTH < rct.right:
         yoko = False
-    if rct.top < 0 or HEIGHT < rct.bottom: 
+    if rct.top < 0 or HEIGHT < rct.bottom:
         tate = False
     return yoko, tate
 
@@ -30,13 +35,13 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
-    bb_img = pg.Surface((20, 20))  
-    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  
-    bb_img.set_colorkey((0, 0, 0))  
-    bb_rct = bb_img.get_rect()  
-    bb_rct.centerx = random.randint(0, WIDTH)  
-    bb_rct.centery = random.randint(0, HEIGHT) 
-    vx, vy = +5, +5  
+    bb_img = pg.Surface((20, 20))
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
+    bb_img.set_colorkey((0, 0, 0))
+    bb_rct = bb_img.get_rect()
+    bb_rct.centerx = random.randint(0, WIDTH)
+    bb_rct.centery = random.randint(0, HEIGHT)
+    vx, vy = +5, +5
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -44,12 +49,14 @@ def main():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
+        if kk_rct.colliderect(bb_rct):
+            return
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for key, mv in DELTA.items():
             if key_lst[key]:
-                sum_mv[0] += mv[0]  
+                sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
         # if key_lst[pg.K_w]:
         #     sum_mv[1] -= 5
@@ -65,13 +72,13 @@ def main():
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
-        bb_rct.move_ip(vx, vy)  
+        bb_rct.move_ip(vx, vy)
         yoko, tate = check_bound(bb_rct)
-        if not yoko:  
+        if not yoko:
             vx *= -1
-        if not tate:  
+        if not tate:
             vy *= -1
-        screen.blit(bb_img, bb_rct)  
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
